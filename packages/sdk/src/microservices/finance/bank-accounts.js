b96a10ec761770911b76cd7a50ffdb1ff2a44258
bissuh <bissuh@gmail.com>
2020-09-01 14:00:27 -0400

    Include banks API and new bank account methods.

--- packages/sdk/src/microservices/finance/bank-accounts.js
+++ packages/sdk/src/microservices/finance/bank-accounts.js
@@ -5,6 +5,28 @@ import { get } from "../../request/request.js";
 import { defaultSettings } from "./base.js";
 
 /**
+ * Create Bank Account to Producer
+ *
+ * @param {object} bankAccount
+ * @param {object} [query]
+ * @param {object} [settings]
+ *
+ * @returns {Promise}
+ */
+function create(
+    bankAccount,
+    query,
+    settings = {}
+) {
+    return post(
+        '/bank-accounts',
+        bankAccount,
+        query,
+        defaultSettings(settings)
+    );
+}
+
+/**
  * Get A Producer's Bank Accounts List
  *
  * @param {object} id
@@ -26,12 +48,35 @@ function list(
 }
 
 /**
+ * Remove Bank Account
+ *
+ * @param {string} id
+ * @param {object} [query]
+ * @param {object} [settings]
+ *
+ * @returns {Promise}
+ */
+function remove(
+    id,
+    query,
+    settings
+) {
+    return del(
+        `/bank-accounts/${id}`,
+        query,
+        defaultSettings(settings)
+    );
+}
+
+/**
  * Reference
  */
 const bankAccounts = {
     defaultSettings,
 
-    list
+    create,
+    list,
+    remove
 };
 
 /**
